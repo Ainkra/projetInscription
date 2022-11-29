951ae5c4469f5295651418165e66f1577358141c
Ainkra <felke2101@gmail.com>
2022-11-29 09:26:28 +0100

    feat: add crypt password method

--- index.php
+++ index.php
@@ -1,25 +1,54 @@
 <?php
 
-use projetInscription\Database;
-use projetInscription\EmailVerif;
+use projetInscription\Database\Database;
+use projetInscription\EmailVerification;
+use projetInscription\PseudoVerification;
+use projetInscription\CryptPassword;
 
-/*Database connection*/
+
+if(!empty($_POST['password']) && !empty($_POST['email']) && !empty($_POST['pseudo'])) {
+    $email = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["password"]);
+    $pseudo = htmlspecialchars($_POST["pseudo"]);
+
+    $db = new Database("localhost", "projet-inscription", "root", "");
+    $emailVerification = new EmailVerification($db);
+    $pseudoVerification = new PseudoVerification($db);
+    $passwordCrypt = new CryptPassword();
+
+    if (!$emailVerification->verifyEmailExist($email)){
+        // L'email n'existe pas, on continue
+        header("location: index.php?success=1&message='email disponible'");
+    }
+    else {
+        // l'email existe, on arrête.
+        header('location: index.php?error=1&message=Email déjà utilisée ! Essayez un autre.');
+        exit();
+    }
+
+    if($pseudoVerification->verifyPseudo($pseudo)) {
+        header("location: index.php?success=1&message='Pseudo valide'");
+    } else {
+        header("location: index.php?error=1&message='Pseudo déjà utilisé. Essayez un autre'");
+        exit();
+    }
+
+    $passwordCrypt->cryptsha1($password);
 
 
-/**/
-if(!empty($_POST['password']) && !empty($_POST['email'])) {
+
 
 }
 
 ?>
 
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="css/default.css">
-	<title>Mon Site PHP</title>
-</head>
+<html lang="fr">
+    <head>
+    	<meta charset="utf-8">
+    	<link rel="stylesheet" href="css/default.css">
+    	<title>Mon Site PHP</title>
+    </head>
     <body>
         <section class="container">
 
@@ -34,9 +63,15 @@ if(!empty($_POST['password']) && !empty($_POST['email'])) {
                     echo '<p class="alert error">'.htmlspecialchars($_GET['message']).'</p>';
                 } ?>
 
+                <label for="pseudo"></label>
                 <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo"><br>
+
+                <label for="email"></label>
                 <input type="email" name="email" id="email" placeholder="Email"><br>
+
+                <label for="password"></label>
                 <input type="password" name="password" id="password" placeholder="Mot de passe"><br>
+
                 <input type="submit" value="Inscription">
 
             </form>
